a6571f794541653bc245408485d795c87b4ada8b
wolstad <wcolstad@gmail.com>
2019-02-18 20:56:35 -0500

    Successful milestone 1

---
+++ code/test-code/milestone_1/milestone_1.ino
@@ -0,0 +1,34 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+// you can also call it with a different address you want
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
+// you can also call it with a different address and I2C interface
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(&Wire, 0x40);
+
+// Depending on your servo make, the pulse width min and max may vary, you 
+// want these to be as small/large as possible without hitting the hard stop
+// for max range. You'll have to tweak them as necessary to match the servos you
+// have!
+#define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("8 channel Servo test!");
+
+  pwm.begin();
+  
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+
+  delay(10);
+}
+
+void loop() {
+  pwm.setPWM(0,0,100);
+  pwm.setPWM(1,0,433);
+//  pwm.setPWM(0,0,333);
+//  pwm.setPWM(1,0,333);
+}
