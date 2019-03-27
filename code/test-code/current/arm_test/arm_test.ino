3712340468635400581b1d16a153ec2c4b747a25
wolstad <43003576+wolstad@users.noreply.github.com>
2019-03-27 15:41:42 -0400

    added claw support

--- code/test-code/current/arm_test/arm_test.ino
+++ code/test-code/current/arm_test/arm_test.ino
@@ -6,6 +6,9 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define ARMSERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
 #define ARMSERVOMAX  350 // this is the 'maximum' pulse length count (out of 4096)
 
+#define CLAWSERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
+#define CLAWSERVOMAX  300 // this is the 'maximum' pulse length count (out of 4096)
+
 
 void setup() {
   Serial.begin(9600);
@@ -18,9 +21,9 @@ void setup() {
 }
 
 void loop() {
-  raise_arm();
+  open_claw();
   delay(3000);
-  lower_arm();
+  close_claw();
   delay(3000);
 }
 
@@ -35,5 +38,19 @@ void lower_arm() {
 void raise_arm() {
   for (uint16_t pulselen = ARMSERVOMIN; pulselen < ARMSERVOMAX; pulselen++) {
     pwm.setPWM(2, 0, pulselen);
+  }
+}
+
+// Lower arm of the robot
+void open_claw() {
+  for (uint16_t pulselen = CLAWSERVOMAX; pulselen > CLAWSERVOMIN; pulselen--) {
+    pwm.setPWM(3, 0, pulselen);
+  }
+}
+
+// Raise arm of the robot
+void close_claw() {
+  for (uint16_t pulselen = CLAWSERVOMIN; pulselen < CLAWSERVOMAX; pulselen++) {
+    pwm.setPWM(3, 0, pulselen);
   }
 }
